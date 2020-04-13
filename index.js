38e5ffa2fbafb9e9fb58ad0547f758810aadbb9a
Zachary Kramer <zckramer83@gmail.com>
2020-04-13 18:44:36 -0400

    initial push

---
+++ index.js
@@ -0,0 +1,26 @@
+class Character {
+    constructor(){
+        name = "";
+        alignment = "";
+    }
+
+
+
+    getName () {
+        return this.name;
+    }
+
+    setName (newName) {
+        this.name = newName;
+    }
+    
+    getAlignment () {
+        return this.name;
+    }
+
+    setAligntment (newAlignment) {
+        this.alignment = newAlignment;
+    }
+}
+
+export default Character
---
+++ index.test.js
@@ -0,0 +1,5 @@
+import UnderTest from './index'
+
+test('returns a string', () => {
+  expect(UnderTest.getName).toBeTruthy();
+});
